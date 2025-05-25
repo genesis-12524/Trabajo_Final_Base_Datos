e8d457e4fd09ef1ec7d7a2967f570c5c638ba617
nicol <genesistafur61@gmail.com>
2025-05-25 02:47:58 -0500

    mi primer commit

---
+++ src/main/java/com/fia/base/ProyectoBaseDatosApplication.java
@@ -0,0 +1,13 @@
+package com.fia.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBaseDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBaseDatosApplication.class, args);
+	}
+
+}
